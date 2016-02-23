67563fbcc6beb2fd1d127d69d1b9551f853deea5
Davide Cesaroni <davide.cesaroni@gmail.com>
2016-02-23 17:40:08 +0100

    Implementa basic logic to check for sender and recipient

--- mail-extractor.js
+++ mail-extractor.js
@@ -2,7 +2,8 @@ var Mbox = require('node-mbox');
 var MailParser = require('mailparser').MailParser;
 
 var mboxFilePath = process.argv[2];
-var senderMailAddress = process.argv[3];
+var inputSenderMailAddress = process.argv[3];
+var inputRecipientMailAddress = process.argv[4];
 var mbox = new Mbox(mboxFilePath);
 
 mbox.on('message', function(msg) {
@@ -11,5 +12,20 @@ mbox.on('message', function(msg) {
   mailparser.on("end", function(mail) {
     console.log("From: ", mail.from);
     console.log("Subject: ", mail.subject);
+    var senders = mail.from;
+    var recipients = mail.to;
+    if (senders.length > 0) {
+      var senderAddress = senders[0].address;
+    }
+
+    if (recipients.length == 1) {
+      console.log("There is only one recipient: " + recipients);
+      if ((senderAddress == inputSenderMailAddress) && (to[0] == inputRecipientMailAddress)) {
+        console.log("The recipient and the sender are who you are expecting", to[0], senderAddress);
+      }
+    } else {
+      console.log("There are many recipients: " + recipients);
+    }
+
   });
 });
